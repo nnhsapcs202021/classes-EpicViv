691c29818edf8ef530ae1aca3fd0833726ef332c
EpicViv <epicviv987@gmail.com>
2020-10-05 15:11:38 -0500

    Finished 10/5 Notes

--- MileageTracker.java
+++ MileageTracker.java
@@ -1,86 +1,134 @@
-
 /**
  * This class models a mileage tracker for a car.
  *
  * @author gcschmit
  * @version 27 September 2020
  */
-public class MileageTracker
-{
-    /**
-     * 1. Define methods by specifying:
-     *      The visibility (e.g., public)
-     *      The return type (e.g., void)
-     *      The method name (e.g., incrementMilesDriven)
-     *      The parameters and their types (e.g., miles of type double)
-     */
+public class MileageTracker {
 
-    /**
-     * Increment the number of miles that the car has drive.
-     * @param miles  the additional distance, in miles, this car has driven
-     */
-    public void incrementMilesDriven(double miles){
-        //!!! TO DO: implement later
-    }
+     /**
+      * 2. Define the instance variables (store the obect's attributes):
+      * Specify the visibility (e.g., private)
+      * Public: accessible by any code in any class
+      * Private: only accessible by methods in this class
+      * Specify the type (e.g., double)
+      * Specify the name (e.g. milesDriven)
+      *
+      * Instance variables differ from local variables in the following ways:
+      * Scoped to the class (accessible in all methods of hte class)
+      * (Lifetime is the same as the object)
+      * Automatically initialized to a default value (0, false, null)
+      * Best practice is not to immediately initialize instance variables
+      */
 
-    /**
-     * Returns the total number of miles driven
-     * @return the total number of miles driven
-     */
-    public double getMilesDriven(){
-        //!!! TO DO: implement later
-        return 0.0;
-    }
+     private double milesDriven; //in units of miles
+     private double fuelConsumed; //in units of gallons
+     private String vin; //vehicle identification number
 
-    /**
-     * Increment the number of gallons of fuel that this car has consumed
-     * @param gallons    the additional fuel, in gallons, this car has consumed
-     */
-    public void incrementFuelConsumed(double gallons)
-    {
-        
-    }
-    
-    /**
-     * Returns the total number of gallons of fuel consumed
-     * @return the total number of gallons of fuel consumed
-     */
-    public double getFuelConsumed()
-    {
-        return 0.0;
-    }
-    
-    
-    /**
-     * Returns the current mileage, in miles per gallon
-     * @return the current mileage, in miles per gallon
-     */
-    public double getMileage()
-    {
-        return 0.0;
-    }
-    
-    /**
-     * Returns the vehicle identification (VIN) of this car.
-     * @return the vehicle identification (VIN) of this car
-     */
-    public String getVIN()
-    {
-        return null;
-    }
-    
-    /**
-     * Sets the vehicle identification (VIN) of this car.
-     * @param newVIN    the vehicle identification (VIN) of this car
-     */
-    public void setVIN(String vin)
-    {
-        vin = vin;
-    }
+     /**
+      * Default constructor for the MileageTracker class.
+      * Initializes the miles driven and fuel consumed to 0 and VIN to null
+      */
+     public MileageTracker(){
+
+          /*
+           * The "this" reserved word references the current object (like "self" in Python).
+           *
+           * Its usage is encouraged but not always required.
+           */
+
+          this.milesDriven = 0;
+          this.fuelConsumed = 0;
+          this.vin = null;
+     }
+
+     /**
+      * Constructs a new MileageTracker object with the specified miles driven and guel consumed.
+      * @param initialMilesDriven the number of miles already driven
+      * @param initialFuelConsumed the number of gallons of fuel already consumed
+      */
+     public MileageTracker(double initialMilesDriven, double initialFuelConsumed){
+          this.milesDriven = initialMilesDriven;
+          this.fuelConsumed = initialFuelConsumed;
+          this.vin = null;
+     }
+
+     /*
+      * 1. Define methods by specifying:
+      *      The visibility (e.g., public)
+      *      The return type (e.g., void)
+      *      The method name (e.g., incrementMilesDriven)
+      *      The parameters and their types (e.g., miles of type double)
+      */
+
+     /**
+      * Increment the number of miles that the car has drive.
+      *
+      * @param miles the additional distance, in miles, this car has driven
+      */
+     public void incrementMilesDriven(double miles) {
+          //TODO: implement later
+     }
+
+     /**
+      * Returns the total number of miles driven
+      *
+      * @return the total number of miles driven
+      */
+     public double getMilesDriven() {
+          //TODO: implement later
+          return 0.0;
+     }
+
+     /**
+      * Increment the number of gallons of fuel that this car has consumed
+      *
+      * @param gallons the additional fuel, in gallons, this car has consumed
+      */
+     public void incrementFuelConsumed(double gallons) {
+
+     }
+
+     /**
+      * Returns the total number of gallons of fuel consumed
+      *
+      * @return the total number of gallons of fuel consumed
+      */
+     public double getFuelConsumed() {
+          return 0.0;
+     }
+
+
+     /**
+      * Returns the current mileage, in miles per gallon
+      *
+      * @return the current mileage, in miles per gallon
+      */
+     public double getMileage() {
+          return 0.0;
+     }
+
+     /**
+      * Returns the vehicle identification (VIN) of this car.
+      *
+      * @return the vehicle identification (VIN) of this car
+      */
+     public String getVIN() {
+          return null;
+     }
+
+     /**
+      * Sets the vehicle identification (VIN) of this car.
+      *
+      * @param vin the vehicle identification (VIN) of this car
+      */
+     public void setVIN(String vin) {
+          vin = vin;
+     }
+
+     public static void main(String[] args){
+
+     }
+
 }
-
-
-
-
-
 
